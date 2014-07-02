05ac4605b1c95f54bd2f2b91c5bbdb7b299fe20e
Hyunmin Kim <hmkim@live.co.kr>
2014-07-03 00:06:56 +0900

    add gatk/gatk_CombineVariants.config.xml

--- gatk/gatk_CombineVariants.pl
+++ gatk/gatk_CombineVariants.pl
@@ -17,7 +17,8 @@ use strict;
 #    are not removed.
 #
 # History:
-#   Version 1.0 (June 28, 2014): first non-beta release.
+#   Version 1.0 (June 28, 2014): just one vcf file processing
+#   Version 1.1 (June 30, 2014): multi vcf file pattern processing
 ##
 
 use File::Basename;
@@ -25,15 +26,68 @@ use Cwd qw(abs_path);
 use lib '/BiOfs/BioPeople/brandon/language/perl/lib/';
 
 use Brandon::Bio::BioTools qw($GATK);
-#use Brandon::Bio::BioPipeline::Project;
-#use Brandon::Bio::BioPipeline::ProcessData;
 use Brandon::Bio::BioTools::GATK;
-#use Brandon::Bio::BioPipeline::Queue;
+use Brandon::Bio::BioPipeline::Queue;
+use Brandon::Bio::BioPipeline::ProcessData;
 
 use Data::Dumper;
 
-use XML::Simple;
 use Getopt::Long;
+use XML::Simple;
+
+###### get arguments      ###
+my ( $config_file, $debug );
+Getopt::Long::Configure("pass_through");
+GetOptions(
+	'config=s' => \$config_file,
+	'debug' => \$debug,
+);
+
+if ( scalar @ARGV ) {
+        warn "Unknown options:", Dumper \@ARGV, "\n";
+        exit 0;
+}
+
+####### general parameters ###
+my $config = XMLin( "$config_file");
+
+my $ref_fasta = $config->{REF_FASTA};
+
+my $set = Brandon::Bio::BioPipeline::ProcessData->new($config->{DATA});
+
+foreach my $vcf_pattern ( @{$set->{data}->{VCFs}} ){
+	my @input_vcf = glob($vcf_pattern);
+	checkFiles(\@input_vcf);
+	my $param = $config->{GATK_CombineVariants_PARAM};
+	my $outdir = $config->{PARAMETERS}->{PROJECT_DIR};
+
+	my $chrName = (split /\./, $vcf_pattern)[-2];
+	my $outfile = $outdir."/".$chrName.".vcf";
+
+	my $gatk_config = {
+		input_file => \@input_vcf,
+		output => $outfile,
+		reference_sequence => $ref_fasta,
+		app => "CombineVariants",
+		param => $param,	
+		java_opts => "-Xmx8g -jar",
+	};
+	my $combine_vcf = Brandon::Bio::BioTools::GATK->new($gatk_config);
+
+	$combine_vcf->{name} = $chrName;
+	my $job_combine_vcf = Brandon::Bio::BioPipeline::Queue->mini($combine_vcf,$debug);
+	$job_combine_vcf->run();
+}
+
+sub checkFiles{
+	my $input_ref = shift;
+
+	foreach (@{$input_ref}){
+		if (!-f $_){
+			die "ERROR! not found $_\n";
+		}
+	}
+}
 
 =pod
  java -Xmx2g -jar GenomeAnalysisTK.jar \
@@ -54,23 +108,4 @@ use Getopt::Long;
    -priority foo,bar
 =cut
 
-my $aa_vcf = "/home/brandon/a.vcf";
-my $bb_vcf = "/home/brandon/b.vcf";
-my $ref_fasta = "/BiOfs/hmkim87/BioResources/Reference/Human/hs37d5/chrAll.fa";
-my @input_vcf;
-push @input_vcf, $aa_vcf;
-push @input_vcf, $bb_vcf;
 
-my $param = "-genotypeMergeOptions UNIQUIFY";
-
-my $outfile = "/home/brandon/merged.gatk.vcf";
-
-my $gatk_config = {
-	input_file => \@input_vcf,
-	output => $outfile,
-	reference_sequence => $ref_fasta,
-	app => "CombineVariants",
-	param => $param, 
-};
-my $combine_vcf = Brandon::Bio::BioTools::GATK->new($gatk_config);
-print Dumper $combine_vcf;
